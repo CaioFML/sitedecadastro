1f7991b491716684665cff6f2bc52871971c9cc2
Unknown <caio.felipemotta@hotmail.com>
2019-02-04 20:38:44 -0200

    Versão 1.8
    Adicionado campo de busca.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
   resources :produtos, only: [:new, :create, :destroy]
   root to: "produtos#index"
 
